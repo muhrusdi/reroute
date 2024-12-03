8e619c79d110d1864d2fd544afe02246a5c50f73
muhrusdi <mhmrofficial@gmail.com>
2024-12-03 13:16:24 +0700

    chore: update rr

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,7 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 import { QueryClientProvider } from "react-query";
-import { queryClient } from "./lib/api/query-client";
+import { queryClient } from "./lib/api/query.client";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
